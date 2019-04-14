621541f42ce01e5d4245aefcdc80d48438e8d0ab
A. M. Walker <b4014242@my.shu.ac.uk>
2019-04-14 23:42:33 +0100

    Test environment setup

--- KRAWLER/KRAWLING/Box2DTest.cpp
+++ KRAWLER/KRAWLING/Box2DTest.cpp
@@ -5,9 +5,53 @@
 #include <Physics\KPhysicsScene.h>
 #include <Utilities\KDebug.h>
 
+#include <Box2D\Box2D.h>
+
 using namespace Krawler;
 using namespace Krawler::Input;
 using namespace Krawler::Components;
+
+
+constexpr int32 VelocityIterations = 6;
+constexpr int32 PositionIterations = 2;
+const b2Vec2 gravity(0.0f, 9.81f);
+
+b2World world(gravity);
+
+class Box2DComp : public KComponentBase
+{
+public:
+
+	Box2DComp(KEntity* pEntity) :
+		KComponentBase(pEntity)
+	{
+
+	}
+
+	virtual KInitStatus init() override
+	{
+		KScene* pScene = GET_SCENE();
+		m_pBox = pScene->addEntityToScene();
+		m_pBox->addComponent(new KCSprite(m_pBox, Vec2f(32, 32)));
+		m_pBox->getTransformComponent()->setTranslation(Vec2f(32, 32));
+
+		return KInitStatus::Success;
+	}
+
+	virtual void onEnterScene() override
+	{
+		m_pBox->getComponent<KCSprite>()->setColour(Colour::White);
+	}
+
+	virtual void tick() override
+	{
+		float dt = KApplication::getApp()->getDeltaTime();
+		world.Step(dt, VelocityIterations, PositionIterations);
+	}
+
+private:
+	KEntity* m_pBox;
+};
 
 #ifndef _CONSOLE
 #include <Windows.h>
@@ -32,6 +76,7 @@ int main(void)
 	app->getSceneDirector().setCurrentScene(KTEXT("SceneA"));
 	auto pCurrentScene = app->getCurrentScene();
 	auto entity = pCurrentScene->addEntityToScene();
+	entity->addComponent(new Box2DComp(entity));
 	//DO STUFF WITH ENTITY HERE
 	InitialiseSubmodules();
 

861993fc14629f4cb04f280ef0abb6124a37388c
A. M. Walker <b4014242@my.shu.ac.uk>
2019-04-14 23:17:07 +0100

    Box 2d added as dependency

---
+++ KRAWLER/KRAWLING/Box2DTest.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <Krawler.h>
+#include <KApplication.h>
+#include <AssetLoader\KAssetLoader.h>
+#include <Physics\KPhysicsScene.h>
+#include <Utilities\KDebug.h>
+
+using namespace Krawler;
+using namespace Krawler::Input;
+using namespace Krawler::Components;
+
+#ifndef _CONSOLE
+#include <Windows.h>
+int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow)
+#else
+int main(void)
+#endif
+{
+	int32 i = sf::Texture::getMaximumSize();
+	KApplicationInitialise initApp(false);
+	initApp.gameFps = 60;
+	initApp.physicsFps = 60;
+	initApp.width = 1024;//sf::VideoMode::getDesktopMode().width;
+	initApp.height = 768; // sf::VideoMode::getDesktopMode().height;
+	initApp.windowStyle = KWindowStyle::Windowed_Fixed_Size;
+	initApp.windowTitle = KTEXT("Testing TiledMaps");
+	StartupEngine(&initApp);
+	const int sceneWidth = KAssetLoader::getAssetLoader().getLevelMap(L"test_level")->width;
+	const int sceneHeight = KAssetLoader::getAssetLoader().getLevelMap(L"test_level")->height;
+	auto app = KApplication::getApp();
+	app->getSceneDirector().addScene(new KScene(std::wstring(KTEXT("SceneA")), Rectf(0, 0, (70 * 32), (40 * 32))));
+	app->getSceneDirector().setCurrentScene(KTEXT("SceneA"));
+	auto pCurrentScene = app->getCurrentScene();
+	auto entity = pCurrentScene->addEntityToScene();
+	//DO STUFF WITH ENTITY HERE
+	InitialiseSubmodules();
+
+	RunApplication();
+
+	ShutdownEngine();
+
+	return 0;
+}
+

1eb21dac3f9bdfc407f56f9f9014ac00771fd157
Mace <AlanMWalker>
2020-06-16 19:17:45 +0100

    Fixed missing namespace error

--- KRAWLER/KRAWLER Engine/src/Maths/KMathsUtilities.cpp
+++ KRAWLER/KRAWLER Engine/src/Maths/KMathsUtilities.cpp
@@ -1,16 +1,16 @@
 #include "Maths/KMathsUtilities.h"
 
-void InitRand()
+void Krawler::Maths::InitRand()
 {
     srand(static_cast<unsigned>(time(0)));
 }
 
-float RandFloat()
+float Krawler::Maths::RandFloat()
 {
     return (static_cast<float> (rand()) / static_cast<float>(RAND_MAX));
 }
 
-float RandFloat(float Max)
+float Krawler::Maths::RandFloat(float Max)
 {
     if (Max != 0)
     {
@@ -19,7 +19,7 @@ float RandFloat(float Max)
     return(0.f);
 }
 
-float RandFloat(float Min, float Max)
+float Krawler::Maths::RandFloat(float Min, float Max)
 {
     if (Max - Min != 0)
     {
@@ -28,7 +28,7 @@ float RandFloat(float Min, float Max)
     return 0.f;
 }
 
-signed int RandInt(signed int val)
+signed int Krawler::Maths::RandInt(signed int val)
 {
     return rand() % val;
 }
